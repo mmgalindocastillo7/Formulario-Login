3d60b64d00ff96b6bd0974741db9592c1608281b
mmgalindocastillo7 <mgalindocastillo6@gmail.com>
2020-12-09 15:19:09 -0500

    Initial commit

---
+++ usuario.php
@@ -0,0 +1,16 @@
+<?php 
+session_start();
+$_SESION['usuario'] = $usuario;
+header("location:index.php")
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Bienvenidos mmgalindo</title>
+</head>
+<body>
+	<h1>Usuarios Registrados<h1>
+	</body>
+	</html>
